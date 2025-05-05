47922a2b5aa6dfded74c5945128962b4a9644530
Sambav <140359924+SambavJetty@users.noreply.github.com>
2025-05-06 00:58:22 +0530

    Update sql-worker.js

--- workers/sql-worker.js
+++ workers/sql-worker.js
@@ -1,16 +1,12 @@
 import { SQLiteFS } from "@aphro/absurd-sql"
 import { IndexedDBBackend } from "@aphro/absurd-sql/dist/indexeddb-backend"
 
-// This is a Web Worker that runs SQL.js with absurd-sql for persistence
 self.addEventListener("message", (event) => {
   const { id, action, args } = event.data
-
   if (action === "init") {
-    // Initialize SQL.js with absurd-sql
     const { SQL } = args
     const sqlFS = new SQLiteFS(SQL.FS, new IndexedDBBackend())
     SQL.register_for_idb(sqlFS)
-
     SQL.FS.mkdir("/sql")
     SQL.FS.mount(sqlFS, {}, "/sql")
 
